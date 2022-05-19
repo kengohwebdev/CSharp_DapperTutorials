a1cb80dbb86a8f040cb497a999b4a0a36282b8f8
megahigh5 <102623149+megahigh5@users.noreply.github.com>
2022-05-19 13:46:29 -0500

    Add files via upload

---
+++ Project3/Color_Ball_Class/Ball.cs
@@ -0,0 +1,56 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+
+//namespace Project3
+//{
+//    public class Ball
+//    {
+//        private int size;
+//        private Color color;
+//        private int numThrows;
+
+//        //constructor for Ball class
+//        public Ball(int size, Color color)
+//        {
+//            this.size = size;
+//            this.color = color;
+//            numThrows = 0;
+//        }
+
+//        //getters and setters for data members
+//        public int getSize()
+//        {
+//            return size;
+//        }
+
+//        public Color getColor()
+//        {
+//            return color;
+//        }
+
+//        public void Pop()
+//        {
+//            size = 0;
+//        }
+
+//        public void Throw()
+//        {
+//            //if ball has not been popped : add the number of throws
+//            if (size != 0)
+//            {
+//                numThrows++;
+//            }
+//        }
+
+//        public int getNumThrows()
+//        {
+//            return numThrows;
+//        }
+
+//    }
+
+//}
---
+++ Project3/Color_Ball_Class/Color.cs
@@ -0,0 +1,79 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+
+
+//namespace Project3
+//{
+//    public class Color
+//    {
+
+
+//        private int red;
+//        private int green;
+//        private int blue;
+//        private int alpha;
+
+        
+
+
+//        //Color constructor
+//        public Color(int red, int green, int blue, int alpha)
+//        {
+//            this.red = red;
+//            this.green = green;
+//            this.blue = blue;
+//            this.alpha = alpha;
+//        }
+//        //getters for data members
+//        public int getRed()
+//        {
+//            return red;
+//        }
+//        public int getGreen()
+//        {
+//            return green;
+//        }
+//        public int getBlue()
+//        {
+//            return blue;
+//        }
+
+//        public int getAlpha()
+//        {
+//            return alpha;
+//        }
+
+//        //setters for data members
+//        public void setRed(int red)
+//        {
+//            this.red = red;
+//        }
+//        public void setGreen(int green)
+//        {
+//            this.green = green;
+//        }
+//        public void setBlue(int blue)
+//        {
+//            this.blue = blue;
+//        }
+
+//        public void setAlpha(int alpha)
+//        {
+//            this.alpha = alpha;
+//        }
+
+//        //get the grayscale value
+//        public double getGrayScaleValue()
+//        {
+//            return ((red + green + blue) * 1.0) / 3;
+//        }
+
+
+//    }
+
+
+//}
