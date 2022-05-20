4dcab9821d22cef8c913496dd38548abf16fd5ee
megahigh5 <102623149+megahigh5@users.noreply.github.com>
2022-05-20 09:49:35 -0500

    Update Department.cs

--- Project3/Person1_6/Department.cs
+++ Project3/Person1_6/Department.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,7 @@ namespace OOP_Persons
 {
      class Department : Instructor
     {
-
+        public String[] courses;
         enum _Courses
         {
             Math,
@@ -28,6 +28,15 @@ namespace OOP_Persons
            this.endDate = endDate;
         }
 
+        public void setCourses(String[] courses)
+        {
+            this.courses = courses;
+        }
+
+        public String[] getCourses()
+        {
+            return this.courses;
+        }
      
 
 
