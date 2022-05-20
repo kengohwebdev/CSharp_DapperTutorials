baa278b53f6388b731a762c692ac531125b79c6b
megahigh5 <102623149+megahigh5@users.noreply.github.com>
2022-05-20 09:48:00 -0500

    Update Instructor.cs

--- Project3/Person1_6/Instructor.cs
+++ Project3/Person1_6/Instructor.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 
 namespace OOP_Persons
 {
-     public class Instructor : Person
+    public class Instructor : Person
     {
         private string buildDepartment;
         private string joinDate;
         private int bonus;
         private Boolean head = false;
+        private String rank;
 
         public Instructor()
         {
@@ -19,7 +20,7 @@ namespace OOP_Persons
         }
         public Instructor(int age, decimal salary, String address,
                            String buildDepartment, int bonus, string joinDate)
-         : base (age, salary, address)
+         : base(age, salary, address)
         {
             this.buildDepartment = buildDepartment;
             this.joinDate = joinDate;
@@ -57,6 +58,16 @@ namespace OOP_Persons
             head = true;
             Console.WriteLine("This person is the head of department!");
             return head;
+        }
+
+        public String getRank()
+        {
+            return rank;
+        }
+
+        public void setRank(String rank)
+        {
+            this.rank = rank;
         }
         public override void Classes()
         {
