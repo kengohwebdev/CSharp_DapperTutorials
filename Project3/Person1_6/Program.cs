11e33cfcd8a5518ec7b5b3baf21ec2065aac67b6
megahigh5 <102623149+megahigh5@users.noreply.github.com>
2022-05-20 10:45:29 -0500

    Update Program.cs

--- Project3/Person1_6/Program.cs
+++ Project3/Person1_6/Program.cs
@@ -33,7 +33,8 @@ namespace OOP
             //downcasting
             Student s1 = new Student();
             s1.age = 21;
-          
+           
+          //Calculate age
             Person p2 = s1;
             Console.WriteLine(p2.age);
 
@@ -47,7 +48,9 @@ namespace OOP
             i1.setDepartment(dep);
             
             i1.getDepartment();
-         
+            
+            
+         //Boolean set instructor to head of department
             i1.isHead();
 
         
@@ -62,15 +65,26 @@ namespace OOP
 
             Student s3 = new Student(18, 0, newAddress, 4, 4.0, grade);
             Person p4 = new Student(19, 0, newAddress, 4, 0, grade);
+            
+            
+            //Calculate GPA with abstract and encapsulation 
+            //s3.AddCourse();
+             s3.getGPA();
 
 
             string jDate = i1.getJoinDate();
-            Person p5 = new Instructor(18, 3500, newAddress, dep, 0, jDate);
+            Person p5 = new Instructor(18, 3500, newAddress, dep, 5000, jDate);
             
+            //Get Salary and address
             Console.WriteLine(p5.salary);
             Console.WriteLine(i1.salary);
-
+            
+            //Calculate Bonus
+            //i1.CalculateBonus();
             i1.getBonus();
+            
+            
+            //for loop to get courses from enum
 
 
 
