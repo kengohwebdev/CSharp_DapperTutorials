4c12694b8b0d9647cc3bf80d3a0eea9b5fe5ddfc
megahigh5 <102623149+megahigh5@users.noreply.github.com>
2022-05-20 02:14:58 -0500

    Add files via upload

---
+++ OOP_Q1-Q6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP;
+using OOP_Persons;
+
+
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person p1;
+            Instructor i = new Instructor();
+            Student s = new Student();
+
+            String dep = "Engineering";
+
+            p1 = new Student();
+            p1.Classes();
+            p1 = new Instructor();
+            p1.Classes();
+            p1.age = 21;
+            Console.WriteLine(p1.age);
+            i.setJoinDate(2020, 4, 20);
+            Console.WriteLine(i.getJoinDate());
+            s.age = 40;
+            Console.WriteLine(s.age);
+            i.setDepartment(dep);
+            Console.WriteLine(i.getDepartment());
+            
+                
+            
+
+        }
+
+
+
+    }
+}
